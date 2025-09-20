b1563c816ba8ccfce32ea99d36c88d7ed585d8d2
ng196 <166632421+ng196@users.noreply.github.com>
2025-09-21 03:37:20 +0530

    Revert "updated events"
    This reverts commit 4370f3345a4c12e40c77e57681366369c02f079c.

--- sangyaan/src/main.jsx
+++ sangyaan/src/main.jsx
@@ -1,20 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>
-);
-
-// Service Worker registration
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/service-worker.js')
-      .then((reg) => console.log('Service Worker registered.', reg))
-      .catch((err) => console.log('Service Worker registration failed:', err));
-  });
-}
+  </StrictMode>,
+)
